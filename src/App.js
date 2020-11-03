413bef84142bfe9195c0e33f5bf75fafb8d273b7
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-11-03 17:34:54 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+import './App.css'
+export default class Facebook extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: ""
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  responseFacebook = response => {
+    console.log(response);
+    if (response) {
+      this.setState({
+        isLoggedIn: true,
+        userID: response.userID,
+        name: response.name,
+        email: response.email,
+        picture: response.picture.data.url
+      });
+    }
+
+  };
+
+  componentClicked = () => console.log("clicked");
+
+  render() {
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = (
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            background: "#f4f4f4",
+            padding: "20px"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="366938184421787"
+          autoLoad={false}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
 
-export default App;
+    return (
+      <div className="main">
+        <div className="container">{fbContent}
+        </div>
+      </div>
+    );
+  }
+}
